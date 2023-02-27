6ae24fad9075f7e84160bb599f1e22f75e5fab9b
SomiaNasir <125132307+SomiaNasir@users.noreply.github.com>
2023-02-27 11:03:01 +0000

    Create Error-Code-Solution.sql

---
+++ Week1 - Error Codes/Error-Code-Solution.sql
@@ -0,0 +1,44 @@
+-- 1
+SELECT email_address, deleted_at
+FROM dsv1069.users 
+WHERE deleted_at IS NULL;
+
+-- 2 
+SELECT category, COUNT(DISTINCT id)
+FROM dsv1069.items 
+GROUP BY category;
+
+-- 3
+SELECT *
+FROM dsv1069.users u JOIN dsv1069.orders o
+ON u.id = o.user_id;
+
+-- 4
+SELECT COUNT(DISTINCT event_id) AS events
+FROM dsv1069.events
+WHERE event_name = 'view_item';
+
+-- 5
+SELECT COUNT(DISTINCT id)
+FROM dsv1069.orders JOIN dsv1069.items 
+ON orders.item_id = items.id;
+
+-- 6 
+SELECT users.id, MIN(orders.created_at) AS first_purchase_at,
+  CASE 
+  WHEN MIN(orders.created_at) IS NULL THEN 'NO'
+  ELSE 'YES'
+  END IF_ORDRED
+FROM dsv1069.users LEFT JOIN dsv1069.orders
+ON users.id = orders.user_id
+GROUP BY users.id;
+
+-- 7 
+SELECT 100 * (
+  SELECT COUNT(DISTINCT users.id) 
+  FROM dsv1069.users JOIN dsv1069.events 
+  ON users.id = events.user_id
+  WHERE events.event_name = 'view_user_profile') /
+  (
+  SELECT COUNT(DISTINCT id) 
+  FROM dsv1069.users) as percentage
